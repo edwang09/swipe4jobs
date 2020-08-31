77a7efacd8efa9accd243c4efc273b624db5df78
Jianan Wang <edwang09@gmail.com>
2020-08-31 14:46:06 -0400

    Updates

---
+++ precache-manifest.16c2d83c01f42444e77870034d67035f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc96fed393b8b8076846bd3692dd0141",
+    "url": "/swipe4jobs/index.html"
+  },
+  {
+    "revision": "e29c5ebb981416c0d67a",
+    "url": "/swipe4jobs/static/css/main.5aae24ae.chunk.css"
+  },
+  {
+    "revision": "60f0028fcb890334374e",
+    "url": "/swipe4jobs/static/js/2.f75501c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/swipe4jobs/static/js/2.f75501c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29c5ebb981416c0d67a",
+    "url": "/swipe4jobs/static/js/main.4dd7ba45.chunk.js"
+  },
+  {
+    "revision": "923e6094d8e05fe7ce14",
+    "url": "/swipe4jobs/static/js/runtime-main.3550f765.js"
+  }
+]);
